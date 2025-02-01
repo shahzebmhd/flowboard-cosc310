8a1aab5d5af38d93631a5ece36c424787b9ed678
Shahzeb <shahzebmhd@gmail.com>
2025-02-01 13:23:42 -0800

    Added Sign-up and Sign-in pages, added react hooks

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,8 @@
+"use client"; 
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 interface AuthLayoutProps {
     children: React.ReactNode
 };
@@ -7,14 +10,21 @@ interface AuthLayoutProps {
 
 
 const AuthLayout = ({children}: AuthLayoutProps) => {
+    const pathname = usePathname();
+    const isSignIn = pathname === "/sign-in";
+    
     return (
         <main className="bg-neutral-100 min-h-screen">
         <div className="mx-auto max-w-screen-2xl p-4">
             <nav className="flex justify-between items-center">
                  <Image src="/logo.svg" height={50} width={50} alt="logo" />
-                    <Button variant="secondary">
-                        Sign up
+                
+                    <Button asChild variant="secondary">
+                        <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                        { isSignIn ? "Sign up" : "Login"}
+                        </Link>
                     </Button>
+               
             </nav>
             <div className="flex flex-col items-center justify-center pt-4 md:pt">
 
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -3,9 +3,12 @@ import {FcGoogle} from "react-icons/fc"
 import {FaGithub} from "react-icons/fa"
 import {Input } from "@/components/ui/input";
 import { DottedSeparator } from "@/components/ui/dotted-separator";
-import { useForm } from "react-hook-form";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod"
+
 import {
     Form,
     FormControl,
@@ -16,42 +19,70 @@ import {
 import {
     Card,
     CardContent, 
+    CardDescription,
     CardHeader,
     CardTitle
 } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
-export const formSchema = z.object({
+const formSchema = z.object({
+    name: z.string().trim().min(1,"Required"),
     email: z.string().email(),
-    password: z.string().min(1, "Required "),
+    password: z.string().min(8, "Minimum of 8 characters required"),
 });
 
-export const SignInCard = () => {
+export const SignUpCard = () => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
+            name:"",
             email: "",
             password: "",
         },
     });
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log({values});
-    } 
+const onSubmit = (values: z.infer<typeof formSchema>)=> {
+    console.log({values});
+};
+    
     return (
         <Card>
             <CardHeader className="flex items-center justify-center text-center p-7">
                 <CardTitle className ="text-2xl">
-                    Welcome Back!
+                    Sign Up
                 </CardTitle>
+                <CardDescription>
+                By signing up, you agree to our {" "}
+                <Link href="/privacy">
+                    <span className="text-blue-700">Privacy Policy</span>
+                </Link>{" "}
+                and{" "}
+                <Link href="/terms">
+                    <span className="text-blue-700">Terms of Service</span>
+                </Link>
+                </CardDescription>
             </CardHeader>
             <div className="px-7">
             <DottedSeparator/>
             </div>
             <CardContent className="p-7">
-                <Form {...form}>
+            <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                    <FormField
+                        name = "name"
+                        control={form.control}
+                        render={({field}) => (
+                            <FormItem>
+                                <FormControl>
+                            <Input
+                            {...field}
+                            type="text"
+                            placeholder="Enter your name"
+                        />
+                            </FormControl>
+                            <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
                     <FormField
                         name = "email"
                         control={form.control}
@@ -86,7 +117,7 @@ export const SignInCard = () => {
                         )}
                     />
                     <Button disabled={false} size="lg" className="w-full">
-                        Login
+                        Sign Up
                     </Button>
                     </form>
                 </Form>    
@@ -120,10 +151,10 @@ export const SignInCard = () => {
             </div>
             <CardContent className="p-7 flex items-center justify-center">
                         <p>
-                            Don&apos;t have an account?
-                            <Link href="/sign-up">
+                            Already have an account?
+                            <Link href="/sign-in">
                             <span className="text-blue-700">
-                            &nbsp;Sign Up
+                            &nbsp;Sign In
                             </span>
                             </Link>
                         </p>
