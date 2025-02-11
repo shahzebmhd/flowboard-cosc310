829d810ef56cf50f11caa80b693f1e6e7ef98446
jessica545 <jessicagoel939@gmail.com>
2025-02-11 15:36:28 -0800

    Implemented route protection  and redirect for unauthenticated users

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,13 @@
-"use client";
-import { SignInCard } from "@/features/auth/components/sign-in-card";
 
-const SignInPage = () => {
+import { SignInCard } from "@/features/auth/components/sign-in-card";
+import { getCurrent } from "@/features/auth/actions";
+import { redirect } from "next/navigation";
+
+const SignInPage =async () => {
+    const user = await getCurrent();
+
+    if (user) redirect("/");
+    
     return (
         <SignInCard/>
     );
