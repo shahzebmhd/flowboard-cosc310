ade11b33efbe9de78d70ed4e2b4a2d66055620de
Germain So <germain717@gmail.com>
2025-02-23 01:19:55 -0800

    workspace-switcher that only load workspacee visible to user(https://localhost:3000/api/workspaces ),generate invite code, link up userId(member relation) on workspace creation

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,16 +2,34 @@ import {Hono} from "hono";
 import {zValidator} from "@hono/zod-validator";
 import {createWorkspaceSchema} from "@/features/workspaces/schemas";
 import {sessionMiddleware} from "@/lib/session-middleware";
-import {DATABASE_ID, IMAGES_BUCKET_ID, WOKRSPACES_ID} from "@/config";
-import {ID} from "node-appwrite";
+import {DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WOKRSPACES_ID} from "@/config";
+import {ID, Query} from "node-appwrite";
+import {MemberRole} from "@/features/members/type";
+import {generateInviteCode} from "@/lib/utils";
 
 const app = new Hono()
     .get("/", sessionMiddleware, async (c) => {
         const databases = c.get("databases");
+        const user = c.get("user");
+
+        const member = await databases.listDocuments(
+            DATABASE_ID,
+            MEMBERS_ID,
+            [Query.equal("userId", user.$id)]
+        )
+
+        if (member.total == 0) {
+            return c.json({data: {documents: [], total: 0}});
+        }
+
+        const workspaceIds = member.documents.map((member) => member.workspaceId);
+
 
         const workspaces = await databases.listDocuments(
             DATABASE_ID,
             WOKRSPACES_ID,
+            [Query.orderDesc("$createdAt"),
+                Query.contains("$id", workspaceIds)]
         );
 
         return c.json({data: workspaces});
@@ -49,8 +67,21 @@ const app = new Hono()
                     name,
                     userId: user.$id,
                     imageUrl: uploadedImageUrl,
+                    inviteCode:generateInviteCode(6),
                 },
             )
+
+            await databases.createDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                ID.unique(),
+                {
+                    userId: user.$id,
+                    workspaceId: workspaces.$id,
+                    role: MemberRole.ADMIN,
+                },
+            )
+
 
             return c.json({data: workspaces});
         }
