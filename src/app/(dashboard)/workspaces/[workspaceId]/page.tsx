2d9cfc82687ce6da8b5d7a8a60da34da977bee38
TriangleMesh <weibodeemail@proton.me>
2025-03-01 02:12:37 -0800

    add task front end page

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -5,8 +5,14 @@ const WorkspaceIdPage = async () => {
     const user = await getCurrent();
     if (!user) redirect("/sign-in");
     return (
-        <div>
-            Workspace Id
+        <div className="w-full h-full">
+            
+            <iframe 
+                src="/index.html" 
+                width="100%" 
+                height="100%" 
+                style={{ border: "none", minHeight: "calc(100vh - 64px)" }} // 64px 是 Navbar 可能占用的高度
+            />
         </div>
     );
 };
