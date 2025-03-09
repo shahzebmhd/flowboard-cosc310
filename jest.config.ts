8dda22051e26c40511b46e02a2914d2a5286b508
sorz.code <germain717@gmail.com>
2025-03-09 01:41:06 -0800

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,12 @@ import type { Config } from 'jest';
 const config: Config = {
     preset: 'ts-jest',
     transform: {
-        '^.+\\.js?$': 'ts-jest',
+        '^.+\\.js?$': 'ts-jest', // Use ts-jest to transform .js files
     },
     testEnvironment: 'node',
+    testMatch: [
+        '**/__tests__/test*.js', // Match test files in the __tests__ directory
+    ],
 };
 
 export default config;
