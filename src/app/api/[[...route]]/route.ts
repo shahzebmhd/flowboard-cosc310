64668a2cb9cb0d2977145a1fd91657f3bfc3a776
jessica545 <jessicagoel939@gmail.com>
2025-03-08 23:28:25 -0800

    FB-3022 -adding-workspace-projects

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,12 +1,14 @@
 import {Hono} from "hono";
 import {handle} from "hono/vercel";
 import auth from "@/features/auth/server/route";
-import workspaces from "@/features/workspaces/server/route"
-
+import workspaces from "@/features/workspaces/server/route";
+import projects from "@/features/projects/server/route";
 const app = new Hono().basePath("/api");
 
 app.route("/auth", auth)
-    .route("/workspaces",workspaces);
+    .route("/workspaces", workspaces)
+    .route("/projects", projects)
+    
 
 export const GET = handle(app);
 export const POST = handle(app);
