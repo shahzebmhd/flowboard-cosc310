9246f3880c5818270f82f125356fa7beb0f301e8
Shahzeb <shahzebmhd@gmail.com>
2025-03-10 12:15:56 -0700

    TaskApi

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,13 +3,15 @@ import {handle} from "hono/vercel";
 import auth from "@/features/auth/server/route";
 import workspaces from "@/features/workspaces/server/route";
 import projects from "@/features/projects/server/route";
+import tasks from "@/features/tasks/server/route";
+
 const app = new Hono().basePath("/api");
 
 app.route("/auth", auth)
     .route("/workspaces", workspaces)
     .route("/projects", projects)
-    
-    
+    .route("/tasks", tasks)
+
 
 export const GET = handle(app);
 export const POST = handle(app);
