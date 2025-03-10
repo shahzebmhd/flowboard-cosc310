eec3870179c8ae45e7deefe38eba83b7ba33ec03
Shahzeb <shahzebmhd@gmail.com>
2025-03-10 12:15:56 -0700

    TaskApi

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -5,6 +5,7 @@ import { ProjectAvatar } from "@/features/projects/components /project-avatar";
 import { Button } from "@/components/ui/button";
 import { PencilIcon } from "lucide-react";
 import Link from "next/link";
+import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher";
 
 interface ProjectIdPageProps {
     params: { projectId: string };
@@ -45,6 +46,7 @@ const ProjectIdPage = async ({
                     </Button>
                 </div>
             </div>
+            <TaskViewSwitcher />
         </div>
     );
 };
