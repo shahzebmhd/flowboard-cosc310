908b8db9e8f16e5dcb0cade2b85338c6e57f1e61
jessica545 <jessicagoel939@gmail.com>
2025-03-28 23:41:56 -0700

    Added the delete functionality

--- src/features/workspaces/components/join-workspace-form.tsx
+++ src/features/workspaces/components/join-workspace-form.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React from "react";
 import { Button } from "@/components/ui/button";
 import{
     Card,
@@ -16,64 +16,55 @@ import { useWorkspaceId } from "../hooks/use-workspace-id";
 import { useRouter } from "next/navigation";
 
 interface JoinWorkspaceFormProps {
-   initialValues: {
-       name: string;
-   };
-};
-
-export const JoinWorkspaceForm = ({
-    initialValues,
-}:JoinWorkspaceFormProps) => {
-    const inviteCode =useInviteCode();
+    initialValues: {
+        name: string;
+    };
+  }
+  export const JoinWorkspaceForm = ({ initialValues }: JoinWorkspaceFormProps) => {
+    const inviteCode = useInviteCode();
     const workspaceId = useWorkspaceId();
     const router = useRouter();
-    const {mutate,isPending} = useJoinWorkspace();
+    const { mutate, isPending } = useJoinWorkspace();
 
     const onSubmit = () => {
-        mutate({
+        mutate(
+          {
             param: { workspaceId },
             json: { code: inviteCode },
-        },{
-            onSuccess: ({data}) => {
-                router.push(`/workspaces/${data.id}`);
+          },
+          {
+            onSuccess: ({ data }) => {
+              router.push(`/workspaces/${data.id}`);
             },
-        })
-    return (
+          }
+        );
+      };
+
+      return (
         <Card className="w-full h-full border-none shadow-none">
-            <CardHeader className="p-7">
-                <CardTitle className="text-xl font-bold">Join Workspace</CardTitle>
+          <CardHeader className="p-7">
+            <CardTitle className="text-xl font-bold">Join Workspace</CardTitle>
             <CardDescription>
-                <p>You&apos;ve been invited to Join<strong>{initialValues.name}</strong> workspace</p>
-            </CardDescription>
-            </CardHeader>
-            <div className="px-7">
+                  <p>
+                    You&apos;ve been invited to join <strong>{initialValues.name}</strong> workspace.
+                  </p>
+                </CardDescription>
+              </CardHeader>
+              <div className="px-7">
                 <DottedSeparator />
-            </div>
-            <CardContent className="p-7">
-             <div className="flex flex-col lg:flex-row gap-2 items-center justify-between">
-                <Button 
-                variant="secondary"
-                type="button"
-                asChild
-                size="lg"
-                className="w-full lg:w-fit"
-                disabled={isPending}>
-                    <Link href="/">
-                    Cancel
-                        </Link>
-                    
-                </Button>
-                <Button 
-                size="lg"
-                className="w-full lg:w-fit"
-                type="button"
-                onClick={onSubmit}
-                disabled={isPending}>
+                </div>
+              <CardContent className="p-7">
+                <div className="flex flex-col lg:flex-row gap-2 items-center justify-between">
+                <Button variant="secondary" type="button" asChild size="lg" className="w-full lg:w-fit" disabled={isPending}>
+                    <Link href="/">Cancel</Link>
+                  </Button>
+                  <Button size="lg" className="w-full lg:w-fit" type="button" onClick={onSubmit} disabled={isPending}>
                     Join Workspace
-                </Button>
-            </div>
-            </CardContent>
-        </Card>
-    )
-}
-}
+                  </Button>
+                </div>
+                </CardContent>
+            </Card>
+          );
+        };
+
+        export default JoinWorkspaceForm;
